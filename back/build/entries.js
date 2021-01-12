4b2a811740aee8bb3e73ed6a06cec4554bfa5e24
Zhi1211 <43130603+Zhi1211@users.noreply.github.com>
2021-01-12 15:16:29 +0800

    以原生 js 重構問卷前台 (#16)
    * 調整專案結構，問卷後台移置 back
    
    * 更新 front gulp 至 4.0.2, 更新 gulpfile
    
    * 加入 使用者檢視問卷頁面
    
    * 更新 front readme.md
    
    * 以原生 js 重構問卷前台

---
+++ back/build/entries.js
@@ -0,0 +1,8 @@
+module.exports = {
+  index: './src/index.entry.js',
+  templateList: './src/templateList.entry.js',
+  template: './src/template.entry.js',
+  questionnaireList: './src/questionnaireList.entry.js',
+  questionnaire: './src/questionnaire.entry.js',
+  create: './src/create.entry.js'
+};
---
+++ front-stage/gulpfile.js
@@ -0,0 +1,99 @@
+const fs = require("fs");
+const gulp = require("gulp");
+const { Storage } = require('@google-cloud/storage');
+const path = require('path');
+
+const bucketNameForTest = "tutor-test-apps";
+const bucketNameForProd = "tutor-apps";
+
+const projectIdTest = "tutor-test-238709";
+const projectIdProd = "tutor-204108";
+const keyFileNameTest = "tutor-test.json";
+const keyFileNameProd = "tutor.json";
+
+const projectName = "app/survey";
+
+const distDir = `${__dirname}/`;
+
+const gcsOptionTest = {
+    projectId: projectIdTest,
+    keyFileName: keyFileNameTest,
+    bucket: bucketNameForTest,
+    cacheControl: 'no-store, no-transform'
+};
+
+const gcsOptionProd = {
+    projectId: projectIdProd,
+    keyFileName: keyFileNameProd,
+    bucket: bucketNameForProd,
+    cacheControl: 'no-store, no-transform'
+};
+
+async function uploadToGCS(gcsOption) {
+    const storage = new Storage({
+        projectId: gcsOption.projectId,
+        keyFileName: "tutor-test.json"
+    });
+    cleanGCS(gcsOption.bucket, storage)
+        .then(async () => {
+            await findUploadFullPaths(distDir).then((paths) => {
+                Promise.all(
+                    paths.map((path) => {
+                        return uploadObject(path, storage, gcsOption);
+                    })
+                );
+            });
+        });
+}
+
+const cleanGCS = async (bucketName, storage) => {
+    const options = {
+        prefix: projectName,
+    };
+    const [files] = await storage.bucket(bucketName).getFiles(options);
+    for (let file of files) {
+        await storage.bucket(bucketName)
+            .file(file.name)
+            .delete();
+        console.log(`${file.name} is deleted`);
+    }
+};
+
+async function findUploadFullPaths(dir, uploadFullPaths = []) {
+    const files = await fs.readdirSync(dir, (err) => { console.error(err); });
+    for (let file of files) {
+        const fullPath = path.join(dir, file);
+        const status = await fs.statSync(fullPath);
+        if(!fullPath.match("node_modules")){
+            if (status.isDirectory()) {
+                uploadFullPaths = await findUploadFullPaths(fullPath, uploadFullPaths);
+            } else {
+                uploadFullPaths.push(fullPath);
+            }     
+        }       
+    }
+    return uploadFullPaths;
+}
+
+function uploadObject(path, storage, gcsOption) {
+    return new Promise((resolve) => {
+        storage.bucket(gcsOption.bucket)
+            .upload(path,
+                {
+                    destination: `${projectName}/${path.replace(distDir, '')}`,
+                    metadata: {
+                        cacheControl: gcsOption.cacheControl,
+                    },
+                    public: true
+                },
+                (err, file) => {
+                    if (err) console.error(err);
+                    console.log(`Upload ${file.name} successfully`);
+                    resolve(file.name);
+                }
+            );
+    });
+}
+
+exports.uploadGcsTest = gulp.series(uploadToGCS.bind(uploadToGCS, gcsOptionTest));
+exports.uploadGcsProd = gulp.series(uploadToGCS.bind(uploadToGCS, gcsOptionProd));
