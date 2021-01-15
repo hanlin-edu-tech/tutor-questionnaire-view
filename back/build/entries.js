e1f62e19a000f1d2bd46dedab6fd02ee03731627
林芷瑄 <fluxion09@gmail.com>
2021-01-15 14:51:21 +0800

    刪除舊的問卷後台

--- back/build/entries.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  index: './src/index.entry.js',
-  templateList: './src/templateList.entry.js',
-  template: './src/template.entry.js',
-  questionnaireList: './src/questionnaireList.entry.js',
-  questionnaire: './src/questionnaire.entry.js',
-  create: './src/create.entry.js'
-};
