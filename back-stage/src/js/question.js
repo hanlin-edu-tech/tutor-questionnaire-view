a9d8e77b643ae0d90b1f66eb251d2408e1f9baad
林芷瑄 <fluxion09@gmail.com>
2021-01-15 17:22:57 +0800

    調整 第一題與第一選項不能刪除，問答題移除選項

--- back-stage/src/js/question.js
+++ back-stage/src/js/question.js
@@ -20,27 +20,26 @@
       }
     </style>
     <div class="form-row">
-  <div class="form-group col-md-6">
+  <div class="form-group col-md-6 topic-col">
     <textarea class="form-control" id="topic" type="text" data-field="topic" rows="1" required="required"></textarea>
     <div class="btn btn-secondary btn-sm" style="margin-top:3px" id="delete-question">刪除</div>
   </div>
-  <div class="form-group col-md-2">
+  <div class="form-group col-md-2 type-col">
     <select class="form-control" id="inputType" data-field="type">
       <option value="單選題">單選題</option>
       <option value="多選題">多選題</option>
       <option value="問答題">問答題</option>
     </select>
   </div>
-  <div class="form-group col-md-3">
+  <div class="form-group col-md-3 option-col">
     <div>
       <div style="display:flex">
-        <input class="form-control" type="text" data-field="options">
-        <ion-icon class="delete-option" name="trash" style="cursor:pointer">                                        </ion-icon>
+        <input class="form-control" type="text" data-field="options">        
       </div>
       <ion-icon class="add-option" name="add-circle-outline" style="cursor:pointer"></ion-icon>
     </div>
   </div>
-  <div class="form-group col-md-1">
+  <div class="form-group col-md-1 required-col">
     <div class="form-check form-check-inline">
       <input class="form-check-input" type="checkbox" value="true" data-field="required"/>是
     </div>
@@ -51,9 +50,13 @@
   customElements.define('a-question', class extends HTMLElement {
     connectedCallback() {
       const questionIndex = this.getAttribute('index');
-      this.shadowRoot.querySelector('#delete-question').addEventListener('click', () => {
-        document.querySelector(`#q${questionIndex}`).remove();
-      });
+      if(questionIndex === '0'){
+        this.shadowRoot.querySelector('#delete-question').remove();
+      }else{
+        this.shadowRoot.querySelector('#delete-question').addEventListener('click', () => {
+          document.querySelector(`#q${questionIndex}`).remove();
+        });
+      }    
     }
 
     constructor() {
@@ -68,20 +71,36 @@
       }
 
       function onClickAddOptionHandler(){
-        shadowRoot.querySelectorAll('.add-option').forEach(it => {
-          it.addEventListener('click', (e) => {
-            const dom = document.createElement('div');
-            dom.innerHTML = `<div style="display:flex">
+        shadowRoot.querySelector('.add-option').addEventListener('click', (e) => {            
+          const dom = document.createElement('div');
+          dom.innerHTML = `<div style="display:flex">
             <input class="form-control" type="text" data-field="options">
             <ion-icon class="delete-option" name="trash" style="cursor:pointer"></ion-icon>
           </div>`;
-            const parent = e.target.parentNode;          
-            parent.insertBefore(dom, parent.lastElementChild);
-            onClickDeleteOptionHandler(dom.querySelector('ion-icon'));
+          const parent = e.target.parentNode;          
+          parent.insertBefore(dom, parent.lastElementChild);
+          onClickDeleteOptionHandler(dom.querySelector('ion-icon'));
           });
-        });
       }
+
+      function onChangeTypeHandler(){
+        shadowRoot.querySelector('#inputType').addEventListener('change', (e) => {
+          const optionCol = shadowRoot.querySelector('.option-col');
+          if(e.target.value === '問答題'){
+            optionCol.innerHTML = '';
+          }else if(e.target.value !== '問答題' && optionCol.innerHTML === ''){
+            optionCol.innerHTML = `<div style="display:flex">
+              <input class="form-control" type="text" data-field="options">            
+            </div>
+            <ion-icon class="add-option" name="add-circle-outline" style="cursor:pointer"></ion-icon>`;
+            onClickAddOptionHandler();
+          }
+          });
+        
+      }
+
       onClickAddOptionHandler();
+      onChangeTypeHandler();
     }
   });
 })();
