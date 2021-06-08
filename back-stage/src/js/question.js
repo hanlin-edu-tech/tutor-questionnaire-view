496c982ed60f3bd5bb4c5d268e87a010a2115d2a
林子豪 <tzhau@ehanlin.com.tw>
2021-06-08 14:23:50 +0800

    新增前端同一題目多個回答欄位 [ch943]

--- back-stage/src/js/question.js
+++ back-stage/src/js/question.js
@@ -1,4 +1,35 @@
 (() => {
+  const selectMenuDom = `<select class="form-control" id="inputType" data-field="type">
+    <option value="單選題">單選題</option>
+    <option value="多選題">多選題</option>
+    <option value="問答題">問答題</option>
+    <option value="下拉選單">下拉選單</option>
+  </select>`;
+
+  const mainDom = `
+    <div class="form-group col-md-6">
+      <textarea class="form-control" id="topic" type="text" data-field="topic" rows="1" required="required"></textarea>
+      <div class="btn btn-secondary btn-sm" style="margin-top:3px" id="delete-question">刪除</div>
+    </div>
+    <div class="form-group col-md-2">
+      ${selectMenuDom}
+      <ion-icon class="add-option-question-type" name="add-circle-outline" style="cursor:pointer"></ion-icon>
+    </div>
+    <div class="form-group col-md-3">
+      <div>
+        <div style="display:flex">
+          <input class="form-control" type="text" data-field="options">
+          <ion-icon class="delete-option" name="trash" style="cursor:pointer"></ion-icon>
+        </div>
+        <ion-icon class="add-option" name="add-circle-outline" style="cursor:pointer"></ion-icon>
+      </div>
+    </div>
+    <div class="form-group col-md-1">
+      <div class="form-check form-check-inline">
+        <input class="form-check-input" type="checkbox" value="true" data-field="required"/>是
+      </div>
+    </div>`;
+
   const createTemplate = () => {
     return `
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" rel="stylesheet">
@@ -19,34 +50,9 @@
           border: 2px solid red !important;
       }
     </style>
-    <div class="form-row">
-  <div class="form-group col-md-6">
-    <textarea class="form-control" id="topic" type="text" data-field="topic" rows="1" required="required"></textarea>
-    <div class="btn btn-secondary btn-sm" style="margin-top:3px" id="delete-question">刪除</div>
-  </div>
-  <div class="form-group col-md-2">
-    <select class="form-control" id="inputType" data-field="type">
-      <option value="單選題">單選題</option>
-      <option value="多選題">多選題</option>
-      <option value="問答題">問答題</option>
-      <option value="下拉選單">下拉選單</option>
-    </select>
-  </div>
-  <div class="form-group col-md-3">
-    <div>
-      <div style="display:flex">
-        <input class="form-control" type="text" data-field="options">
-        <ion-icon class="delete-option" name="trash" style="cursor:pointer">                                        </ion-icon>
-      </div>
-      <ion-icon class="add-option" name="add-circle-outline" style="cursor:pointer"></ion-icon>
+    <div class="form-row" id=0>
+      ${mainDom}
     </div>
-  </div>
-  <div class="form-group col-md-1">
-    <div class="form-check form-check-inline">
-      <input class="form-check-input" type="checkbox" value="true" data-field="required"/>是
-    </div>
-  </div>
-</div>
     `;
   };
   customElements.define('a-question', class extends HTMLElement {
@@ -62,6 +68,11 @@
       const shadowRoot = this.attachShadow({ mode: 'open' });
       shadowRoot.innerHTML = createTemplate();
 
+      // let formList = document.querySelectorAll('.form-row');
+      // let length = formList.length;
+      // let id = formList[length-1].id + 1;
+      // shadowRoot.querySelector('.form-row').id = id;
+
       function onClickDeleteOptionHandler(dom){
         dom.addEventListener('click', (e) => {        
           e.target.parentNode.remove();
@@ -70,19 +81,39 @@
 
       function onClickAddOptionHandler(){
         shadowRoot.querySelectorAll('.add-option').forEach(it => {
-          it.addEventListener('click', (e) => {
-            const dom = document.createElement('div');
+          it.removeEventListener('click', onClickFunHandler ,true);
+          it.addEventListener('click', onClickFunHandler, true);
+        });
+      }
+
+      const onClickFunHandler = (e) => {
+        const dom = document.createElement('div');
             dom.innerHTML = `<div style="display:flex">
             <input class="form-control" type="text" data-field="options">
             <ion-icon class="delete-option" name="trash" style="cursor:pointer"></ion-icon>
           </div>`;
-            const parent = e.target.parentNode;          
-            parent.insertBefore(dom, parent.lastElementChild);
-            onClickDeleteOptionHandler(dom.querySelector('ion-icon'));
+        const parent = e.target.parentNode;          
+        parent.insertBefore(dom, parent.lastElementChild);
+        onClickDeleteOptionHandler(dom.querySelector('ion-icon'));
+      }
+
+      function onClickAddQuestionTypeHandler(){
+        shadowRoot.querySelectorAll('.add-option-question-type').forEach(it => {
+          it.addEventListener('click', (e) => {
+            let form = document.createElement('div');
+            form.classList.add('form-row');
+            form.innerHTML = mainDom;
+            shadowRoot.appendChild(form);
+            let topic = form.querySelector('#topic');
+            topic.disabled = true
+            topic.value = '與上同題';
+            onClickAddOptionHandler();
+            form.querySelector('.add-option-question-type').remove();
           });
         });
       }
       onClickAddOptionHandler();
+      onClickAddQuestionTypeHandler();
     }
   });
 })();
