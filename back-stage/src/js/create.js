c9a89ac54d335f408c9b17a68eb4bb9a2e63f507
林子豪 <tzhau@ehanlin.com.tw>
2021-06-16 10:34:01 +0800

    新增api下拉選單選項 [ch1108]

--- back-stage/src/js/create.js
+++ back-stage/src/js/create.js
@@ -162,6 +162,11 @@
             errMsg += '多選題至少要有二個以上的選項 \n';
           }
         }
+        if (q.type === '下拉選單' || q.type === 'api 下拉選單' && q.options.length <= 1) {
+          if (errMsg.indexOf('下拉選單至少要有一個以上的選項 \n') === -1) {
+            errMsg += '下拉選單至少要有一個以上的選項 \n';
+          }
+        }
         if (q.options) {
           q.options.forEach(o => {
             if (!o) {
